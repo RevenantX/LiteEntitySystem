18d130a6631c4c2da7be3f489a3be32e594c61d1
Bruno Logerfo <bclogerfo@hotmail.com>
2023-09-24 11:44:11 -0300

    added docs

--- LiteEntitySystem/Extensions/SyncArray.cs
+++ LiteEntitySystem/Extensions/SyncArray.cs
@@ -18,6 +18,10 @@ namespace LiteEntitySystem.Extensions
         
         public int Length => _data.Length;
 
+        /// <summary>
+        /// Changes to this array will NOT sync,
+        /// so it should be used as readonly!
+        /// </summary>
         public T[] Value => _data;
 
         public SyncArray(int size)
