3f7bfa2b9e3ad004d411ba6628f9eb642af040a4
Ruslan Pyrch <doomrevx@gmail.com>
2023-10-03 11:50:41 +0300

    remove unused usings

--- LiteEntitySystem/Internal/EntityClassData.cs
+++ LiteEntitySystem/Internal/EntityClassData.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 namespace LiteEntitySystem.Internal
 {
