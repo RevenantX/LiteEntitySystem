04626fce2af332931ce8072452d85898c6fb4b58
Ruslan Pyrch <doomrevx@gmail.com>
2024-04-18 20:52:07 +0300

    minify transport

--- LiteEntitySystem/Transport/LiteNetLibNetPeer.cs
+++ LiteEntitySystem/Transport/LiteNetLibNetPeer.cs
@@ -13,43 +13,17 @@ namespace LiteEntitySystem.Transport
             if(assignToTag)
                 NetPeer.Tag = this;
         }
-        
-        public override void TriggerSend()
-        {
-            NetPeer.NetManager.TriggerUpdate();
-        }
 
-        public override void SendReliableOrdered(ReadOnlySpan<byte> data)
-        {
-            NetPeer.Send(data, 0, DeliveryMethod.ReliableOrdered);
-        }
-
-        public override void SendUnreliable(ReadOnlySpan<byte> data)
-        {
-            NetPeer.Send(data, 0, DeliveryMethod.Unreliable);
-        }
-
-        public override int GetMaxUnreliablePacketSize()
-        {
-            return NetPeer.GetMaxSinglePacketSize(DeliveryMethod.Unreliable);
-        }
-        
-        public override string ToString()
-        {
-            return NetPeer.ToString();
-        }
+        public override void TriggerSend() => NetPeer.NetManager.TriggerUpdate();
+        public override void SendReliableOrdered(ReadOnlySpan<byte> data) => NetPeer.Send(data, 0, DeliveryMethod.ReliableOrdered);
+        public override void SendUnreliable(ReadOnlySpan<byte> data) => NetPeer.Send(data, 0, DeliveryMethod.Unreliable);
+        public override int GetMaxUnreliablePacketSize() => NetPeer.GetMaxSinglePacketSize(DeliveryMethod.Unreliable);
+        public override string ToString() => NetPeer.ToString();
     }
 
     public static class LiteNetLibExtensions
     {
-        public static LiteNetLibNetPeer GetLiteNetLibNetPeerFromTag(this NetPeer peer)
-        {
-            return (LiteNetLibNetPeer)peer.Tag;
-        }
-
-        public static LiteNetLibNetPeer GetLiteNetLibNetPeer(this NetPlayer player)
-        {
-            return (LiteNetLibNetPeer)player.Peer;
-        }
+        public static LiteNetLibNetPeer GetLiteNetLibNetPeerFromTag(this NetPeer peer) => (LiteNetLibNetPeer)peer.Tag;
+        public static LiteNetLibNetPeer GetLiteNetLibNetPeer(this NetPlayer player) => (LiteNetLibNetPeer)player.Peer;
     }
 }
