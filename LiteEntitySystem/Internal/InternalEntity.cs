489988a4dc439dc6226bc0359bff55074ee7ca95
Ruslan Pyrch <doomrevx@gmail.com>
2024-07-06 20:16:28 +0300

    better namings and createRpc overloads

--- LiteEntitySystem/Internal/InternalEntity.cs
+++ LiteEntitySystem/Internal/InternalEntity.cs
@@ -230,7 +230,7 @@ namespace LiteEntitySystem.Internal
         protected void ExecuteRPC<T>(in RemoteCallSpan<T> rpc, ReadOnlySpan<T> value) where T : unmanaged => rpc.Call(this, value);
         
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        protected void ExecuteRPC<T1, T2>(in RemoteCall<T1, T2> rpc, T1 value1, ReadOnlySpan<T2> value2) 
+        protected void ExecuteRPC<T1, T2>(in RemoteCallValueSpan<T1, T2> rpc, T1 value1, ReadOnlySpan<T2> value2) 
             where T1 : unmanaged 
             where T2 : unmanaged => 
             rpc.Call(this, value1, value2);
--- LiteEntitySystem/RPCRegistrator.cs
+++ LiteEntitySystem/RPCRegistrator.cs
@@ -40,10 +40,10 @@ namespace LiteEntitySystem
             (classPtr, buffer, _) => methodToCall((TClass)classPtr, MemoryMarshal.Cast<byte, T>(buffer));
     }
     
-    public struct RemoteCall<T1, T2> where T1 : unmanaged where T2 : unmanaged
+    public struct RemoteCallValueSpan<T1, T2> where T1 : unmanaged where T2 : unmanaged
     {
         internal Delegate CachedAction;
-        internal static RemoteCall<T1, T2> Create<TClass>(ValueSpanAction<TClass, T1, T2> action) => new() { CachedAction = action };
+        internal static RemoteCallValueSpan<T1, T2> Create<TClass>(ValueSpanAction<TClass, T1, T2> action) => new() { CachedAction = action };
         internal void Call<TClass>(TClass self, T1 data1, ReadOnlySpan<T2> data2) => ((ValueSpanAction<TClass, T1, T2>)CachedAction)?.Invoke(self, data1, data2);
         internal static unsafe MethodCallDelegate CreateMCD<TClass>(ValueSpanAction<TClass, T1, T2> methodToCall) =>
             (classPtr, buffer1, buffer2) =>
@@ -151,6 +151,32 @@ namespace LiteEntitySystem
         }
         
         /// <summary>
+        /// Creates cached rpc action with valueType and Span argument
+        /// </summary>
+        /// <param name="self">Target entity with RPC just for auto type deducting</param>
+        /// <param name="methodToCall">RPC method to call</param>
+        /// <param name="remoteCallHandle">output handle that should be used to call rpc</param>
+        /// <param name="flags">RPC execution flags</param>
+        public void CreateRPCAction<TEntity, T1, T2>(TEntity self, ValueSpanAction<TEntity, T1, T2> methodToCall, ref RemoteCallValueSpan<T1, T2> remoteCallHandle, ExecuteFlags flags) 
+            where T1 : unmanaged
+            where T2 : unmanaged
+            where TEntity : InternalEntity =>
+            CreateRPCAction(methodToCall, ref remoteCallHandle, flags);
+
+        /// <summary>
+        /// Creates cached rpc action with Span argument
+        /// </summary>
+        /// <param name="self">Target entity with RPC just for auto type deducting</param>
+        /// <param name="methodToCall">RPC method to call</param>
+        /// <param name="remoteCallHandle">output handle that should be used to call rpc</param>
+        /// <param name="flags">RPC execution flags</param>
+        public void CreateRPCAction<TEntity, T1, T2>(TEntity self, SpanAction<TEntity, T1, T2> methodToCall, ref RemoteCallSpan<T1, T2> remoteCallHandle, ExecuteFlags flags) 
+            where T1 : unmanaged
+            where T2 : unmanaged
+            where TEntity : InternalEntity =>
+            CreateRPCAction(methodToCall, ref remoteCallHandle, flags);
+        
+        /// <summary>
         /// Creates cached rpc action
         /// </summary>
         /// <param name="methodToCall">RPC method to call</param>
@@ -231,16 +257,16 @@ namespace LiteEntitySystem
         /// <param name="methodToCall">RPC method to call</param>
         /// <param name="remoteCallHandle">output handle that should be used to call rpc</param>
         /// <param name="flags">RPC execution flags</param>
-        public unsafe void CreateRPCAction<TEntity, T1, T2>(ValueSpanAction<TEntity, T1, T2> methodToCall, ref RemoteCall<T1, T2> remoteCallHandle, ExecuteFlags flags) 
+        public unsafe void CreateRPCAction<TEntity, T1, T2>(ValueSpanAction<TEntity, T1, T2> methodToCall, ref RemoteCallValueSpan<T1, T2> remoteCallHandle, ExecuteFlags flags) 
             where T1 : unmanaged
             where T2 : unmanaged
             where TEntity : InternalEntity
         {
             ushort rpcId = (ushort)_calls.Count;
-            _calls.Add(new RpcFieldInfo(RemoteCall<T1, T2>.CreateMCD(methodToCall)));
+            _calls.Add(new RpcFieldInfo(RemoteCallValueSpan<T1, T2>.CreateMCD(methodToCall)));
             if (remoteCallHandle.CachedAction != null)
                 return;
-            remoteCallHandle = RemoteCall<T1, T2>.Create<InternalEntity>((e,v1,v2) =>
+            remoteCallHandle = RemoteCallValueSpan<T1, T2>.Create<InternalEntity>((e,v1,v2) =>
             {
                 if (e.EntityManager.IsServer)
                 {
@@ -346,16 +372,16 @@ namespace LiteEntitySystem
             });
         }
         
-        public unsafe void CreateClientAction<TSyncField, T1, T2>(ValueSpanAction<TSyncField, T1, T2> methodToCall, ref RemoteCall<T1, T2> remoteCallHandle)
+        public unsafe void CreateClientAction<TSyncField, T1, T2>(ValueSpanAction<TSyncField, T1, T2> methodToCall, ref RemoteCallValueSpan<T1, T2> remoteCallHandle)
             where T1 : unmanaged
             where T2 : unmanaged
             where TSyncField : SyncableField
         {
             ushort rpcId = _internalRpcCounter++;
-            _calls.Add(new RpcFieldInfo(_syncableOffset, RemoteCall<T1, T2>.CreateMCD(methodToCall)));
+            _calls.Add(new RpcFieldInfo(_syncableOffset, RemoteCallValueSpan<T1, T2>.CreateMCD(methodToCall)));
             if (remoteCallHandle.CachedAction != null)
                 return;
-            remoteCallHandle = RemoteCall<T1, T2>.Create<SyncableField>((s, value1, value2) =>
+            remoteCallHandle = RemoteCallValueSpan<T1, T2>.Create<SyncableField>((s, value1, value2) =>
             {
                 s.ServerEntityManager?.AddRemoteCall(s.ParentEntityInternal.Id, new ReadOnlySpan<T1>(&value1, 1), value2, (ushort)(rpcId + s.RPCOffset), s.Flags);
             });
