53a4083bea7e9778f43d3c7596f84df716398c90
RevenantX <doomrevx@gmail.com>
2024-09-09 13:47:13 +0300

    add both

--- LiteEntitySystem/Extensions/SyncSpanSerializable.cs
+++ LiteEntitySystem/Extensions/SyncSpanSerializable.cs
@@ -1,13 +1,11 @@
-﻿using System;
+using System;
 using K4os.Compression.LZ4;
 using LiteNetLib.Utils;
 
 namespace LiteEntitySystem.Extensions
 {
-    public class SyncNetSerializable<T> : SyncableField where T : INetSerializable
+    public class SyncSpanSerializable<T> : SyncableField where T : ISpanSerializable
     {
-        private static readonly NetDataWriter WriterCache = new();
-        private static readonly NetDataReader ReaderCache = new();
         private static byte[] CompressionBuffer;
         
         private T _value;
@@ -26,7 +24,7 @@ namespace LiteEntitySystem.Extensions
 
         private readonly Func<T> _constructor;
 
-        public SyncNetSerializable(Func<T> constructor)
+        public SyncSpanSerializable(Func<T> constructor)
         {
             _constructor = constructor;
         }
@@ -36,26 +34,22 @@ namespace LiteEntitySystem.Extensions
             r.CreateClientAction(this, Init, ref _initAction);
         }
 
-        protected internal override void OnSyncRequested()
+        protected internal override unsafe void OnSyncRequested()
         {
-            WriterCache.Reset();
-            _value.Serialize(WriterCache);
-            if (WriterCache.Length > ushort.MaxValue)
+            var spanWriter = new SpanWriter(stackalloc byte[_value.MaxSize]);
+            _value.Serialize(spanWriter);
+            if (spanWriter.Position > ushort.MaxValue)
             {
                 Logger.LogError("Too much sync data!");
                 return;
             }
-            int bufSize = LZ4Codec.MaximumOutputSize(WriterCache.Length) + 2;
+            int bufSize = LZ4Codec.MaximumOutputSize(spanWriter.Position) + 2;
             if(CompressionBuffer == null || CompressionBuffer.Length < bufSize)
                 CompressionBuffer = new byte[bufSize];
-            FastBitConverter.GetBytes(CompressionBuffer, 0, (ushort)WriterCache.Length);
+            FastBitConverter.GetBytes(CompressionBuffer, 0, (ushort)spanWriter.Position);
             int encodedLength = LZ4Codec.Encode(
-                WriterCache.Data,
-                0,
-                WriterCache.Length,
-                CompressionBuffer,
-                2,
-                CompressionBuffer.Length-2,
+                spanWriter.RawData.Slice(0, spanWriter.Position),
+                new Span<byte>(CompressionBuffer, 2, CompressionBuffer.Length-2),
                 LZ4Level.L00_FAST);
             ExecuteRPC(_initAction, new ReadOnlySpan<byte>(CompressionBuffer, 0, encodedLength+2));
         }
@@ -66,12 +60,11 @@ namespace LiteEntitySystem.Extensions
             if (CompressionBuffer == null || CompressionBuffer.Length < origSize)
                 CompressionBuffer = new byte[origSize];
             LZ4Codec.Decode(data[2..], new Span<byte>(CompressionBuffer));
-            ReaderCache.SetSource(CompressionBuffer, 0, origSize);
             _value ??= _constructor();
-            _value.Deserialize(ReaderCache);
+            _value.Deserialize(new SpanReader(new ReadOnlySpan<byte>(CompressionBuffer, 0, origSize)));
         }
 
-        public static implicit operator T(SyncNetSerializable<T> field)
+        public static implicit operator T(SyncSpanSerializable<T> field)
         {
             return field._value;
         }
