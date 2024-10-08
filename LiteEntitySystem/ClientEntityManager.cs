a979e8c4eb9074f2ea649c91df015b3effd82afb
Dima <pinotml@gmail.com>
2024-10-08 18:03:09 +0300

    Fixed first baseline sync if data is empty, fixed incorrect header size check

--- LiteEntitySystem/ClientEntityManager.cs
+++ LiteEntitySystem/ClientEntityManager.cs
@@ -268,7 +268,7 @@ namespace LiteEntitySystem
             {
                 if (inData[1] == InternalPackets.BaselineSync)
                 {
-                    if (inData.Length < sizeof(BaselineDataHeader) + 2)
+                    if (inData.Length < sizeof(BaselineDataHeader))
                         return DeserializeResult.Error;
                     _entitiesToConstructCount = 0;
                     _syncCallsCount = 0;
@@ -276,7 +276,7 @@ namespace LiteEntitySystem
                     //read header and decode
                     int decodedBytes;
                     var header = *(BaselineDataHeader*)rawData;
-                    if (header.OriginalLength <= 0)
+                    if (header.OriginalLength < 0)
                         return DeserializeResult.Error;
                     _serverSendRate = (ServerSendRate)header.SendRate;
 
@@ -332,7 +332,7 @@ namespace LiteEntitySystem
                 }
                 else
                 {
-                    if (inData.Length < sizeof(DiffPartHeader) + 2)
+                    if (inData.Length < sizeof(DiffPartHeader))
                         return DeserializeResult.Error;
                     var diffHeader = *(DiffPartHeader*)rawData;
                     int tickDifference = Utils.SequenceDiff(diffHeader.Tick, _lastReadyTick);
