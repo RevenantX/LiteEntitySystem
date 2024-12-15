fb26367184a0df2005213a367ab77eed5febbc3e
RevenantX <doomrevx@gmail.com>
2024-12-15 12:09:23 +0200

    remove uneeded checks

--- LiteEntitySystem/SyncableField.cs
+++ LiteEntitySystem/SyncableField.cs
@@ -48,7 +48,7 @@ namespace LiteEntitySystem
         protected void ExecuteRPC(in RemoteCall rpc)
         {
             if(IsServer)
-                ParentEntityInternal?.ServerManager.AddRemoteCall(ParentEntityInternal, (ushort)(rpc.Id + RPCOffset), rpc.Flags);
+                ParentEntityInternal.ServerManager.AddRemoteCall(ParentEntityInternal, (ushort)(rpc.Id + RPCOffset), rpc.Flags);
         }
 
         protected void ExecuteRPC<T>(in RemoteCall<T> rpc, T value) where T : unmanaged
@@ -56,14 +56,14 @@ namespace LiteEntitySystem
             unsafe
             {
                 if(IsServer)
-                    ParentEntityInternal?.ServerManager.AddRemoteCall(ParentEntityInternal, new ReadOnlySpan<T>(&value, 1), (ushort)(rpc.Id + RPCOffset), Flags);
+                    ParentEntityInternal.ServerManager.AddRemoteCall(ParentEntityInternal, new ReadOnlySpan<T>(&value, 1), (ushort)(rpc.Id + RPCOffset), Flags);
             }
         }
 
         protected void ExecuteRPC<T>(in RemoteCallSpan<T> rpc, ReadOnlySpan<T> value) where T : unmanaged
         {
             if(IsServer)
-                ParentEntityInternal?.ServerManager.AddRemoteCall(ParentEntityInternal, value, (ushort)(rpc.Id + RPCOffset), Flags);
+                ParentEntityInternal.ServerManager.AddRemoteCall(ParentEntityInternal, value, (ushort)(rpc.Id + RPCOffset), Flags);
         }
 
         protected void ExecuteRPC<T>(in RemoteCallSerializable<T> rpc, T value) where T : struct, ISpanSerializable
@@ -72,7 +72,7 @@ namespace LiteEntitySystem
             {
                 var writer = new SpanWriter(stackalloc byte[value.MaxSize]);
                 value.Serialize(ref writer);
-                ParentEntityInternal?.ServerManager.AddRemoteCall<byte>(ParentEntityInternal, writer.RawData.Slice(0, writer.Position), (ushort)(rpc.Id + RPCOffset), Flags);
+                ParentEntityInternal.ServerManager.AddRemoteCall<byte>(ParentEntityInternal, writer.RawData.Slice(0, writer.Position), (ushort)(rpc.Id + RPCOffset), Flags);
             }
         }
     }
