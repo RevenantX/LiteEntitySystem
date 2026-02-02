c3f777bbebb95dcd25d8c6562c316af093e69985
RevenantX <doomrevx@gmail.com>
2026-02-02 12:49:30 +0200

    Update LiteNetLib to 2.0.1 (fix #35)

--- LiteEntitySystem/Transport/LiteNetLibNetPeer.cs
+++ LiteEntitySystem/Transport/LiteNetLibNetPeer.cs
@@ -5,9 +5,9 @@ namespace LiteEntitySystem.Transport
 {
     public class LiteNetLibNetPeer : AbstractNetPeer
     {
-        public readonly NetPeer NetPeer;
+        public readonly LiteNetPeer NetPeer;
         
-        public LiteNetLibNetPeer(NetPeer netPeer, bool assignToTag)
+        public LiteNetLibNetPeer(LiteNetPeer netPeer, bool assignToTag)
         {
             NetPeer = netPeer;
             if(assignToTag)
@@ -15,15 +15,15 @@ namespace LiteEntitySystem.Transport
         }
 
         public override void TriggerSend() => NetPeer.NetManager.TriggerUpdate();
-        public override void SendReliableOrdered(ReadOnlySpan<byte> data) => NetPeer.Send(data, 0, DeliveryMethod.ReliableOrdered);
-        public override void SendUnreliable(ReadOnlySpan<byte> data) => NetPeer.Send(data, 0, DeliveryMethod.Unreliable);
+        public override void SendReliableOrdered(ReadOnlySpan<byte> data) => NetPeer.Send(data, DeliveryMethod.ReliableOrdered);
+        public override void SendUnreliable(ReadOnlySpan<byte> data) => NetPeer.Send(data, DeliveryMethod.Unreliable);
         public override int GetMaxUnreliablePacketSize() => NetPeer.GetMaxSinglePacketSize(DeliveryMethod.Unreliable);
         public override string ToString() => NetPeer.ToString();
     }
 
     public static class LiteNetLibExtensions
     {
-        public static LiteNetLibNetPeer GetLiteNetLibNetPeerFromTag(this NetPeer peer) => (LiteNetLibNetPeer)peer.Tag;
+        public static LiteNetLibNetPeer GetLiteNetLibNetPeerFromTag(this LiteNetPeer peer) => (LiteNetLibNetPeer)peer.Tag;
         public static LiteNetLibNetPeer GetLiteNetLibNetPeer(this NetPlayer player) => (LiteNetLibNetPeer)player.Peer;
     }
 }
